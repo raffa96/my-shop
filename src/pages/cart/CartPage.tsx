8993541e82cc6a3ccd1129df0b6437eacda0bf22
Raffaele Tufano <raffaele@thebubblecompany.com>
2023-05-11 23:16:47 +0200

    Aggiunte funzionalità allo stato del carrello e completato layout pagina carello

--- src/pages/cart/CartPage.tsx
+++ src/pages/cart/CartPage.tsx
@@ -1,11 +1,71 @@
+import {
+  selectCartIsEmpty,
+  selectCartItems,
+  selectTotalCartCost,
+  useCart,
+} from "@/services/cart";
+import { NavLink } from "react-router-dom";
+
 export const CartPage = () => {
+  const cartItems = useCart(selectCartItems);
+  const totalCartCost = useCart(selectTotalCartCost);
+  const cartIsEmpty = useCart(selectCartIsEmpty);
+
+  const increaseQuantity = useCart((state) => state.increaseQuantity);
+  const decreaseQuantity = useCart((state) => state.decreaseQuantity);
+
   return (
     <>
       <h1 className="title">Cart</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quas
-        maxime eius laboriosam hic accusantium illo officia non eos animi.
-      </p>
+      <ul>
+        {cartItems.map(({ product, quantity }) => (
+          <li
+            key={product.id}
+            className="flex flex-col items-center justify-between gap-3 py-3 border-b border-blue-400 sm:flex-row"
+          >
+            <div className="flex items-center gap-3">
+              <img
+                src={product.tmb}
+                alt={product.name}
+                className="object-contain w-24 h-24 rounded-xl"
+              />
+              <div className="font-bold">{product.name}</div>
+            </div>
+
+            <div className="flex flex-col items-center gap-4 sm:flex-row">
+              <div className="flex items-center gap-3">
+                <button
+                  className="btn primary"
+                  onClick={() => decreaseQuantity(product.id)}
+                >
+                  -
+                </button>
+                <div>Quantity: {quantity}</div>
+                <button
+                  className="btn primary"
+                  onClick={() => increaseQuantity(product.id)}
+                >
+                  +
+                </button>
+              </div>
+              <div className="w-16 text-center">
+                € {quantity * product.price}
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <div className="my-4 mr-4 text-4xl text-right">
+        Total: € {totalCartCost}
+      </div>
+
+      {!cartIsEmpty && (
+        <div className="flex justify-center">
+          <NavLink to="/checkout" className="btn primary lg">
+            Confirm order
+          </NavLink>
+        </div>
+      )}
     </>
   );
 };
--- src/services/cart/useCart.ts
+++ src/services/cart/useCart.ts
@@ -16,19 +16,45 @@ export const useCart = create<CartState>((set, get) => ({
   addToCart: (product: Product) => {
     const found = get().items.find(({ product: p }) => p.id === product.id);
     if (found) {
+      get().increaseQuantity(product.id);
+    } else {
+      const item: CartItem = { product, quantity: 1 };
+      set({ items: [...get().items, item] });
+    }
+  },
+  increaseQuantity: (productId: string) => {
+    const found = get().items.find(({ product }) => product.id === productId);
+    if (found) {
       found.quantity++;
       set((state) => ({
         items: state.items.map((item) => {
           return item.product.id === found.product.id ? found : item;
         }),
       }));
-    } else {
-      const item: CartItem = { product, quantity: 1 };
-      set({ items: [...get().items, item] });
     }
   },
-  increaseQuantity: (productId: string) => {},
-  decreaseQuantity: (productId: string) => {},
-  removeFromCart: (productId: string) => {},
-  clearCart: () => {},
+  decreaseQuantity: (productId: string) => {
+    const found = get().items.find(({ product }) => product.id === productId);
+
+    if (found && found.quantity === 1) {
+      get().removeFromCart(productId);
+    }
+
+    if (found && found.quantity > 0) {
+      found.quantity--;
+      set((state) => ({
+        items: state.items.map((item) => {
+          return item.product.id === found.product.id ? found : item;
+        }),
+      }));
+    }
+  },
+  removeFromCart: (productId: string) => {
+    set((state) => ({
+      items: state.items.filter((item) => item.product.id !== productId),
+    }));
+  },
+  clearCart: () => {
+    set({ items: [] });
+  },
 }));
